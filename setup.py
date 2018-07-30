89ea7ee80c37188e0917fda097d8559036622ec4
Diogo Munaro <diogo.munaro@corp.globo.com>
2018-07-30 11:12:45 -0300

    certifi higher or equal

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="gsenha",
-    version="0.0.5",
+    version="0.0.6",
     description="client for gsenha api",
     author="Big Data",
     author_email="bigdata@corp.globo.com",
     license='MIT',
     install_requires=[
         "asn1crypto==0.22.0",
-        "certifi==2017.4.17",
+        "certifi>=2017.4.17",
         "cffi==1.10.0",
         "chardet==3.0.4",
         "cryptography==1.9",
