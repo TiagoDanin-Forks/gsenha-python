995267580dc206110d7378eea48a31389292e112
Diogo Munaro <diogo.munaro@corp.globo.com>
2018-12-04 18:43:33 -0200

    Fixing security issues

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gsenha",
-    version="0.0.6",
+    version="0.0.7",
     description="client for gsenha api",
     author="Big Data",
     author_email="bigdata@corp.globo.com",
@@ -14,12 +14,12 @@ setup(
         "certifi>=2017.4.17",
         "cffi==1.10.0",
         "chardet==3.0.4",
-        "cryptography==1.9",
+        "cryptography>=2.3",
         "enum34==1.1.6",
         "idna==2.5",
         "ipaddress==1.0.18",
         "pycparser==2.18",
-        "requests==2.18.1",
+        "requests>=2.20.0",
         "six==1.10.0",
         "urllib3==1.21.1"
     ],
