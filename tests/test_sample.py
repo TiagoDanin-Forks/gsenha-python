b08f0214f873e504bff5f318bf2b9a27b9347717
Hélio Correia <dev@heliocorreia.com>
2019-10-10 01:40:12 -0300

    test: key load error

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,10 @@
+import pytest
+
 from gsenha import PasswordManager
 
 
-def test_dummy():
-    assert not isinstance([], PasswordManager)
+def test_key_load_error():
+    with pytest.raises(Exception) as excinfo:
+        PasswordManager(key='')
+    assert "key load error" in str(excinfo.value)
 
